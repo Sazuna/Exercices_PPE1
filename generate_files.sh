be0db4bfb1228775f45a08b11b093d32a54f28cd
Sazuna <liza.fretel@gmail.com>
2022-10-12 12:06:21 +0200

    Yearly annotations function and call

---
+++ generate_files.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+./yearly_annotations.sh 2016 2017 2018
--- premier_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "hello !"
-
-ls Fichiers/*.ann
-ls Fichiers/2016*.ann > 2016.txt
-ls Fichiers/2017*.ann > 2017.txt
-ls Fichiers/2018*.ann > 2018.txt
-
