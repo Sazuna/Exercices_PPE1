0ea99ef897686c4b247eed90036fb1ac83a5a82d
Sazuna <liza.fretel@gmail.com>
2022-11-16 11:32:19 +0100

    test si l'url est valide

--- exercises/2022_11_09/get_url.sh
+++ exercises/2022_11_09/get_url.sh
@@ -8,14 +8,20 @@ then
 fi
 
 echo "getting URLs of $2..."
-for URL in $(cat $2)
+#for URL in $(cat $2)
+while read -p text URL;
 do
-	echo "url : $URL"
-	FILE="$WORD/$OUTPUT_NUMBER.txt"
-	curl -i $URL > $FILE
-	RESPONSE=$(./analyze_response.sh $FILE)
-	echo $RESPONSE
+	if [[ "$URL" =~ ^https?:// ]]
+	then
+		echo "url : $URL"
+		FILE="$WORD/$OUTPUT_NUMBER.txt"
+		curl -i $URL > $FILE
+		RESPONSE=$(./analyze_response.sh $FILE)
+		echo $RESPONSE
 
-	echo "$RESPONSE" > $WORD/$OUTPUT_NUMBER\_res.txt
-	OUTPUT_NUMBER=$(expr $OUTPUT_NUMBER + 1 )
-done
+		echo "$RESPONSE" > $WORD/$OUTPUT_NUMBER\_res.txt
+		OUTPUT_NUMBER=$(expr $OUTPUT_NUMBER + 1 )
+	else
+		echo "$URL is not a valid url."
+	fi
+done < $2
